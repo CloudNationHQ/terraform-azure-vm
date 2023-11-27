d11f38f7951daabf313a4abb5839005f4da3b54d
Jorrit05 <jorstut@hotmail.com>
2023-11-27 16:00:34 +0200

    fix: added managed identity (#3)
    * fix: added managed identity
    
    * fix: added managed identity
    
    * fix: added managed identity

--- terraform.tf
+++ terraform.tf
@@ -6,5 +6,15 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.61"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
   }
 }
