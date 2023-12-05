7d2cb10296edb7390c2150d9fdd5420027b3bc95
dkool <dkool@dkools-MacBook-Pro.local>
2023-12-05 16:36:57 +0100

    feat: add custom data and cloud init example

---
+++ examples/custom-data/README.md
@@ -0,0 +1,44 @@
+This example details the setup and utilization of custom data and cloud-init.
+
+```hcl
+module "vm" {
+  source  = "cloudnationhq/vm/azure"
+  version = "~> 0.1"
+
+  keyvault   = module.kv.vault.id
+  naming     = local.naming
+  depends_on = [module.kv]
+
+  vm = {
+    type          = "linux"
+    name          = module.naming.linux_virtual_machine.name
+    resourcegroup = module.rg.groups.demo.name
+    location      = module.rg.groups.demo.location
+
+    custom_data = <<EOF
+I2Nsb3VkLWNvbmZpZwpwYWNrYWdlX3VwZ3JhZGU6IHRydWUKcGFja2FnZXM6CiAg
+LSBhcGFjaGUyCnJ1bmNtZDoKICAtIGVjaG8gIjxodG1sPjxib2R5PjxoMT5XZWxj
+b21lIHRvIE15IFdlYiBQYWdlPC9oMT48L2JvZHk+PC9odG1sPiIgPiAvdmFyL3d3
+dy9odG1sL2luZGV4Lmh0bWwK
+EOF
+
+    interfaces = {
+      int = {
+        subnet = module.network.subnets.int.id
+      }
+    }
+  }
+```
+
+As a reference the below cloud-init.yaml file can be encoded with ```base64 -i cloud-init.yaml```.
+
+```yaml
+#cloud-config
+package_upgrade: true
+packages:
+  - apache2
+runcmd:
+  - echo "<html><body><h1>Welcome to My Web Page</h1></body></html>" > /var/www/html/index.html
+```
+
+This base64 encoded string can be used as input for the custom data property.
---
+++ examples/custom-data/main.tf
@@ -0,0 +1,80 @@
+module "naming" {
+  source  = "cloudnationhq/naming/azure"
+  version = "~> 0.1"
+
+  suffix = ["demo", "dev"]
+}
+
+module "rg" {
+  source  = "cloudnationhq/rg/azure"
+  version = "~> 0.1"
+
+  groups = {
+    demo = {
+      name   = module.naming.resource_group.name
+      region = "westeurope"
+    }
+  }
+}
+
+module "network" {
+  source  = "cloudnationhq/vnet/azure"
+  version = "~> 0.1"
+
+  naming = local.naming
+
+  vnet = {
+    name          = module.naming.virtual_network.name
+    location      = module.rg.groups.demo.location
+    resourcegroup = module.rg.groups.demo.name
+    cidr          = ["10.18.0.0/16"]
+
+    subnets = {
+      int = {
+        cidr = ["10.18.1.0/24"]
+      }
+    }
+  }
+}
+
+module "kv" {
+  source  = "cloudnationhq/kv/azure"
+  version = "~> 0.1"
+
+  naming = local.naming
+
+  vault = {
+    name          = module.naming.key_vault.name_unique
+    location      = module.rg.groups.demo.location
+    resourcegroup = module.rg.groups.demo.name
+  }
+}
+
+module "vm" {
+  source  = "cloudnationhq/vm/azure"
+  version = "~> 0.1"
+
+  keyvault   = module.kv.vault.id
+  naming     = local.naming
+  depends_on = [module.kv]
+
+  vm = {
+    type          = "linux"
+    name          = module.naming.linux_virtual_machine.name
+    resourcegroup = module.rg.groups.demo.name
+    location      = module.rg.groups.demo.location
+
+    custom_data = <<EOF
+I2Nsb3VkLWNvbmZpZwpwYWNrYWdlX3VwZ3JhZGU6IHRydWUKcGFja2FnZXM6CiAg
+LSBhcGFjaGUyCnJ1bmNtZDoKICAtIGVjaG8gIjxodG1sPjxib2R5PjxoMT5XZWxj
+b21lIHRvIE15IFdlYiBQYWdlPC9oMT48L2JvZHk+PC9odG1sPiIgPiAvdmFyL3d3
+dy9odG1sL2luZGV4Lmh0bWwK
+EOF
+
+    interfaces = {
+      int = {
+        subnet = module.network.subnets.int.id
+      }
+    }
+  }
+}
