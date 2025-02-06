4e9fbee2ccfd575020f7b8f2d056d6c2757744ab
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2025-02-06 09:20:06 +0100

    fix: revert default value linux virtual machines and improved availability set iteration (#174)

--- modules/availability-sets/main.tf
+++ modules/availability-sets/main.tf
@@ -1,6 +1,6 @@
 # availability sets
 resource "azurerm_availability_set" "avail" {
-  for_each = try(var.availability_sets, {})
+  for_each = var.availability_sets != null ? var.availability_sets : {}
 
   name                         = try(each.value.name, join("-", [var.naming.availability_set, each.key]))
   location                     = try(each.value.location, var.location)
@@ -10,5 +10,7 @@ resource "azurerm_availability_set" "avail" {
   platform_update_domain_count = try(each.value.platform_update_domain_count, 5)
   proximity_placement_group_id = try(each.value.proximity_placement_group_id, null)
 
-  tags = try(each.value.tags, var.tags, {})
+  tags = try(
+    each.value.tags, var.tags, {}
+  )
 }
