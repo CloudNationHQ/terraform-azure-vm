090f3df3f7197efdc0ecfa52c1ce5d4cc21301a8
dkooll <dennis.kool@cloudnation.nl>
2025-04-23 10:14:41 +0200

    feat: changing settings format extensions

--- main.tf
+++ main.tf
@@ -568,13 +568,23 @@ resource "azurerm_virtual_machine_extension" "ext" {
   # settings                    = try(each.value.settings, null) != null ? jsonencode(each.value.settings) : null
   # protected_settings          = try(each.value.protected_settings, null) != null ? jsonencode(each.value.protected_settings) : null
 
-  settings = try(each.value.settings, null) != null ? (
-    can(tostring(each.value.settings)) ? each.value.settings : jsonencode(each.value.settings)
-  ) : null
+  settings = try(
+    each.value.settings == null ? null : (
+      can(tostring(each.value.settings)) && !can(each.value.settings.fields) ?
+      each.value.settings :
+      jsonencode(each.value.settings)
+    ),
+    null
+  )
 
-  protected_settings = try(each.value.protected_settings, null) != null ? (
-    can(tostring(each.value.protected_settings)) ? each.value.protected_settings : jsonencode(each.value.protected_settings)
-  ) : null
+  protected_settings = try(
+    each.value.protected_settings == null ? null : (
+      can(tostring(each.value.protected_settings)) && !can(each.value.protected_settings.fields) ?
+      each.value.protected_settings :
+      jsonencode(each.value.protected_settings)
+    ),
+    null
+  )
 
   provision_after_extensions  = each.value.provision_after_extensions
   failure_suppression_enabled = each.value.failure_suppression_enabled
