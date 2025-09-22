a8de255c547ed2381dd9f9b83be8d1fbef368793
Jasper Post <jasper.post@cloudnation.nl>
2025-09-22 15:09:09 +0200

    fix: extension settings default null (#215)

--- main.tf
+++ main.tf
@@ -564,8 +564,8 @@ resource "azurerm_virtual_machine_extension" "ext" {
   type                       = each.value.type
   type_handler_version       = each.value.type_handler_version
   auto_upgrade_minor_version = each.value.auto_upgrade_minor_version
-  settings                   = jsonencode(each.value.settings)
-  protected_settings         = jsonencode(each.value.protected_settings)
+  settings                   = length(try(each.value.settings, {})) > 0 ? jsonencode(each.value.settings) : null
+  protected_settings         = length(try(each.value.protected_settings, {})) > 0 ? jsonencode(each.value.protected_settings) : null
 
   provision_after_extensions  = each.value.provision_after_extensions
   failure_suppression_enabled = each.value.failure_suppression_enabled
