a60978b5ddb93d26ca51e38d269d7a8553b9aac2
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2026-03-23 19:44:57 +0100

    feat: add missing outputs (#232)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "network_interfaces" {
   description = "contains all network interfaces config"
   value       = azurerm_network_interface.this
 }
+
+output "disks" {
+  description = "contains all managed disks config"
+  value       = azurerm_managed_disk.this
+}
+
+output "disk_attachments" {
+  description = "contains all data disk attachments config"
+  value       = azurerm_virtual_machine_data_disk_attachment.this
+}
+
+output "extensions" {
+  description = "contains all virtual machine extensions config"
+  value       = azurerm_virtual_machine_extension.this
+}
